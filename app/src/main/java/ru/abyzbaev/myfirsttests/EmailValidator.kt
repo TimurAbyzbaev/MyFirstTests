b1c1cffe6ef3f3dc055e7f0b4824451d00843c3a
TimurHomePC <timstudent15@gmail.com>
2023-10-15 12:01:52 +0500

    Initial commit

---
+++ app/src/main/java/ru/abyzbaev/myfirsttests/EmailValidator.kt
@@ -0,0 +1,35 @@
+package ru.abyzbaev.myfirsttests
+
+import android.text.Editable
+import android.text.TextWatcher
+import java.util.regex.Pattern
+
+class EmailValidator : TextWatcher {
+    internal var isValid = false
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+
+    override fun afterTextChanged(editableText: Editable?) {
+        isValid = isValidEmail(editableText)
+    }
+
+    private fun isValidEmail(email: CharSequence?): Boolean {
+        return email != null && EMAIL_PATTERN.matcher(email).matches()
+    }
+
+    companion object {
+        /**
+         * Паттерн для сравнения.
+         */
+        private val EMAIL_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+    }
+}
---
+++ app/src/main/java/ru/abyzbaev/myfirsttests/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.abyzbaev.myfirsttests
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ru.abyzbaev.myfirsttests.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val emailValidator = EmailValidator()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            emailInput.addTextChangedListener(emailValidator)
+
+            saveButton.setOnClickListener {
+                if (emailValidator.isValid) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.valid_email),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    var errorEmail = getString(R.string.invalid_email)
+                    emailInput.error = errorEmail
+                    Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+    }
+}
